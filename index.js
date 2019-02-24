7571b68d4c494cecda7565afef67f1e407a6b1e7
jocular-markup-2001 <andreacannuni@gmail.com>
2019-02-24 15:48:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens;
 }
+
+// Add your functions and code here
+function destructivelyPrependKitten(name) {
+  kittens.shift(name);
+  return kittens;
+}
